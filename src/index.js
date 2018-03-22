87caa00ef1e8dc0b9508a3bfe452e8dd3f3b6fca
Thomas_Johnson <tjohnson1106@gmail.com>
2018-03-21 21:37:44 -0400

    created basic redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { reducer as formReducer } from "redux-form";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+const rootReducer = combineReducers({
+  form: formReducer
+});
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
