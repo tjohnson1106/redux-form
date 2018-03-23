608d588967aa2d160bfc5b9bf6ed76f5dab34f3c
Thomas_Johnson <tjohnson1106@gmail.com>
2018-03-22 20:12:22 -0400

    added email, age name validation

--- src/App.js
+++ src/App.js
@@ -129,6 +129,33 @@ let SignInForm = props => {
   );
 };
 
+const validate = val => {
+  const errors = {};
+  if (!val.firstName) {
+    console.log("First name is required");
+    errors.firstName = "Required";
+  }
+  if (!val.lastName) {
+    console.log("Last name is required");
+    errors.lastName = "Required";
+  }
+  if (!val.email) {
+    console.log("email name is required");
+    errors.email = "Required";
+  } else if (!/^.+@.+$/i.test(val.email)) {
+    console.log("email is invalid");
+    errors.email = "Invaldi email address";
+  }
+  if (!val.age) {
+    errors.age = "Required";
+  } else if (isNaN(Number(val.age))) {
+    errors.age = "Must be a number";
+  } else if (Number(val.age) < 18) {
+    errors.age = "Sorry, you must be at least 18 years old";
+  }
+  return errors;
+};
+
 SignInForm = reduxForm({
   form: "signIn"
 })(SignInForm);
